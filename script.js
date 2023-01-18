7318d23774e0c62a1189a4d450287a1a5dee8b57
onlinity <116060268+onlinity@users.noreply.github.com>
2023-01-18 16:17:57 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
         /*Time countdown*/
 
-        export const activities = {
+        const activities = {
             'Izjašnjavanje učenika iz kog predmeta će raditi treći test.' : new Date('Dec 31, 2022').getTime(),
             'Provera tačnosti izjašnjenja iz kog predmeta će učenici raditi treći test.': new Date('Jan 27, 2023').getTime(),
             'Promena odluke o trećem testu.': new Date('Jan 31, 2023 15:00:00').getTime(),
@@ -113,7 +113,7 @@
 
     /*Brisanje prošlih događaja iz dict*/
 
-        sada = new Date('May 15, 2023').getTime()
+        sada = new Date().getTime()
         for (const događaj of Object.values(dict)){
             if (događaj < sada){
                 delete dict[key_for_value(dict, događaj)]
@@ -158,3 +158,90 @@
         const datum_sledece = dobijanje_datuma(Math.min.apply(Math, Object.values(dict)))
         document.getElementById('datum').innerHTML = datum_sledece
 
+
+        /*From file script2.js*/
+
+        
+        const dict2 = activities
+    
+        /*function key_for_value(dictionary, value) {
+            return Object.keys(dictionary).find(key => dictionary[key] === value);
+        }
+    
+    
+    
+        function all_keys_for_value(dictionary, value) {
+            return Object.keys(dictionary).filter(key => dictionary[key] === value);
+        }
+    
+    
+    
+    
+    
+    
+    
+        function dobijanje_datuma(p){
+            const godina = new Date(p).getFullYear()
+            const mesec = new Date(p).getMonth() + 1
+            const dan = new Date(p).getDate()
+            const konacno = dan + '. ' + mesec + '. ' + godina + '.   '
+            return konacno
+        }
+    
+    
+        Brisanje prošlih događaja iz dict2
+            sada = new Date('May 15, 2023').getTime()
+            for (const događaj of Object.values(dict2)){
+                if (događaj < sada){
+                    delete dict2[key_for_value(dict2, događaj)]
+                }
+            }*/
+
+            /*Dobijanje rečnika bez već prikazanog događaja*/
+            l = all_keys_for_value(dict, Math.min.apply(Math, Object.values(dict)))
+            for (const i of l){
+                delete dict2[i]
+            }
+
+
+
+            /*Menjanje teksta sledećih aktivnosti*/
+    
+            lista2 = all_keys_for_value(dict2, Math.min.apply(Math, Object.values(dict2)))
+            if (lista2.length == 1){
+                const tekst_sledeće2 = lista2[0]
+                document.getElementById('a').innerHTML = tekst_sledeće2
+            }
+            if (lista2.length == 2){
+                const tekst_sledeće2 = lista2[0]
+                document.getElementById('a').innerHTML = tekst_sledeće2
+                const tekst_sledeće2_1 = lista2[1]
+                document.getElementById('a1').innerHTML = tekst_sledeće2_1
+            }
+            if (lista2.length == 3){
+                const tekst_sledeće2 = lista2[0]
+                document.getElementById('a').innerHTML = tekst_sledeće2
+                const tekst_sledeće2_1 = lista2[1]
+                document.getElementById('a1').innerHTML = tekst_sledeće2_1
+                const tekst_sledeće2_2 = lista2[2]
+                document.getElementById('a2').innerHTML = tekst_sledeće2_2
+            }
+            if (lista2.length == 4){
+                const tekst_sledeće2 = lista2[0]
+                document.getElementById('a').innerHTML = tekst_sledeće2
+                const tekst_sledeće2_1 = lista2[1]
+                document.getElementById('a1').innerHTML = tekst_sledeće2_1
+                const tekst_sledeće2_2 = lista2[2]
+                document.getElementById('a2').innerHTML = tekst_sledeće2_2
+                const tekst_sledeće2_3 = lista2[3]
+                document.getElementById('a3').innerHTML = tekst_sledeće2_3
+            }
+    
+    
+    
+            /*Menjanje datuma sledeće aktivnosti*/
+            const datum_sledece2 = dobijanje_datuma(Math.min.apply(Math, Object.values(dict2)))
+            document.getElementById('d').innerHTML = datum_sledece2
+
+    
+    
